e24f5582e5f16238d4575a6b32c9752517d90372
Nova-IV <sofiane.zaidi@outlook>
2025-05-07 09:37:20 +0200

    start

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+    <link rel="stylesheet" href="./css/main.css">
+
+</head>
+<body>
+    <header>
+        <h1>Formulaire Admin</h1>
+    </header>
+    <main>
+         
+    <fieldset>
+        <form action="traitement.php" method="POST">
+            
+
+        </form>
+    </fieldset>
+
+    </main>
+
+    <footer>
+        <p>
+            $copy; - PHP - 2025
+        </p>
+    </footer>
+</body>
+</html>
